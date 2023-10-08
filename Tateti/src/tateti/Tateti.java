6b4f7b57293bcce6344e416217a543caaa5af881
CintiaBarroso <147344803+CintiaBarroso@users.noreply.github.com>
2023-10-08 16:42:45 -0300

    Mi Primer juego en java
    Un juego sesillo de tatetí

---
+++ Tateti/src/tateti/Tateti.java
@@ -0,0 +1,17 @@
+
+package tateti;
+
+/**
+ *
+ * @author cinti
+ */
+public class Tateti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
